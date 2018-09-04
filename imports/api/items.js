abb32240bf2e1ca4d58a91c5cb822f9f75608d17
Matt Swanson <matt@minnesotabolt.com>
2018-09-04 07:45:29 -0500

    Make item history include aliases

--- imports/api/items.js
+++ imports/api/items.js
@@ -8,4 +8,15 @@ if(Meteor.isServer){
     const itemId = new Mongo.ObjectID(id);
     return Items.find(itemId);
   });
+
+  // Need to publish the item aliases else they wont be found on client side
+  Meteor.publish('itemAliases', function(id){
+    const baseId = new Mongo.ObjectID(id);
+    const baseItem = Items.findOne(baseId);
+    let aliasIds = baseItem.aliases.reduce((acc, val)=>{
+      acc.push(val.refId);
+      return acc;
+    }, []);
+    return Items.find({ _id: { $in: aliasIds }});
+  });
 }
--- imports/ui/items/viewer.js
+++ imports/ui/items/viewer.js
@@ -19,6 +19,7 @@ Template.itemViewer.onCreated(function(){
     FlowRouter.watchPathChange();
     let id = FlowRouter.getParam('id');
     Meteor.subscribe('singleItem', id);
+    Meteor.subscribe('itemAliases', id);
   })
 });
 
@@ -26,6 +27,39 @@ Template.itemViewer.helpers({
   item(){
     const id = FlowRouter.getParam('id');
     const itemId = new Mongo.ObjectID(id);
-    return Items.findOne(itemId);
+    return Items.findOne({ _id: itemId}, { history: { $slice: 10 } });
+  },
+
+  // Get item history which includes it's aliases
+  aHistory(){
+
+    // Get this items history
+    const id = FlowRouter.getParam('id');
+    const itemId = new Mongo.ObjectID(id);
+    const baseItem = Items.findOne({_id: itemId});
+    const aliases = baseItem.aliases;
+    let history = baseItem.history; //.fetch();
+    history.forEach((shipment) => {
+      shipment.itemNumber = baseItem.number;
+      shipment.itemRevision = baseItem.revision;
+    });
+    // Step through each alias
+    aliases.forEach((alias) => {
+      let ai = Items.findOne({_id: alias.refId});
+      //  -  get its history
+      let aliasHistory = ai.history;
+      aliasHistory.forEach((shipment) => {
+        shipment.itemNumber = alias.number;
+        shipment.itemRevision = alias.revision;
+      });
+      // Combine all histories into one array
+      history = history.concat(aliasHistory);
+    });
+    // Sort the array by shipment date and then return it to the template
+    history.sort((a,b)=>{
+      return b.date - a.date;
+    });
+    
+    return history;
   }
 })
