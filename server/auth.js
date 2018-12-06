cc838e49f87df2f8a30d6fb31115372b2de6e70f
Matt Swanson <matt@minnesotabolt.com>
2018-12-06 14:41:30 -0600

    Hide nav stuff to un loged peoples

---
+++ server/auth.js
@@ -0,0 +1,15 @@
+import { SimpleSchema } from 'simpl-schema/dist/SimpleSchema';
+
+Accounts.validateNewUser((user) => {
+  new SimpleSchema({
+    _id: { type: String },
+    emails: { type: Array },
+    'emails.$': { type:Object },
+    'emails.$.address': { type: String },
+    'emails.$.verified': { type: Boolean },
+    createdAt: { type: Date },
+    services: { type: Object, blackbox: true }
+  }).validate(user);
+
+  return true;
+});
